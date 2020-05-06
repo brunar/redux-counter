e296dc2e50a1c6037353ccbfcbfe2fdb9585dfa7
Bruna Radeke <brunaradeke@hotmail.com>
2020-05-06 21:49:03 +0100

    Updating State Immutably (14-263)
    ... spread operator

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,25 +1,36 @@
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'INCREMENT':
-            return {
-                counter: state.counter + 1
-            }
+            const newState = Object.assign({}, state);
+            newState.counter = state.counter + 1
+            return newState;
         //do not need break because return
         case 'DECREMENT':
             return {
+                ...state,
                 counter: state.counter - 1
             }
         case 'ADD':
             return {
+                ...state,
                 counter: state.counter + action.val
             }
         case 'SUBTRACT':
             return {
+                ...state,
                 counter: state.counter - action.val
+            }
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({ id: new Date(), value: state.counter })
+                // concat returns a new array which is the older array plus the argument you add to concat - immutable way to update an array
+                // and push manipulates the original value
             }
     }
     return state;
