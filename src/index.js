841cca9261ba4c7a2a7f1ed8aed613f62b021a34
Bruna Radeke <brunaradeke@hotmail.com>
2020-05-13 10:09:33 +0100

    Redux Advanced - Using the Redux Devtools (16-285)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 
 import './index.css';
@@ -24,8 +24,10 @@ const logger = store => {
         }
     }
 }
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 //Adding ApplyMiddleware as 2nd Argument so-called enhancer
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(
+    rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
