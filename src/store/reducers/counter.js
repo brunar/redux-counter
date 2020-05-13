58e2372f3f2e0c1d323295ba74a9f3bf50ea590d
Bruna Radeke <brunaradeke@hotmail.com>
2020-05-13 19:53:26 +0100

    Redux Adv - Using Utility Functions (16-293)

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     counter: 0
@@ -7,25 +8,24 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.INCREMENT:
-            const newState = Object.assign({}, state);
-            newState.counter = state.counter + 1
-            return newState;
+            //And a sortcode of the return same code as above and using a function that comes from utility.js
+            return updateObject(state, { counter: state.counter + 1 })
+        //Long aproach of update state
+        // const newState = Object.assign({}, state);
+        // newState.counter = state.counter + 1
+        // return newState;
         //do not need break because return
         case actionTypes.DECREMENT:
-            return {
-                ...state,
-                counter: state.counter - 1
-            }
+            return updateObject(state, { counter: state.counter - 1 })
+        //Other aproach of update state
+        // return {
+        //     ...state,
+        //     counter: state.counter - 1
+        // }
         case actionTypes.ADD:
-            return {
-                ...state,
-                counter: state.counter + action.val
-            }
+            return updateObject(state, { counter: state.counter + action.val })
         case actionTypes.SUBSTRACT:
-            return {
-                ...state,
-                counter: state.counter - action.val
-            }
+            return updateObject(state, { counter: state.counter - action.val })
     }
     return state;
 }
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     results: []
@@ -8,22 +9,24 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
             //or here can change data 
-            return {
-                ...state,
-                results: state.results.concat({ id: new Date(), value: action.resultAct })
-                //results: state.results.concat({ id: new Date(), value: action.resultAct * 3 })
-                //have cases where you really want to change something before you store it in the state,
-                //Can change it in the Reducer(better way because is about state) ex multiple value result * 3 as u can see it above or in Actions has another way example
+            return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.resultAct }) })
+        // concat returns a new array which is the older array plus the argument you add to concat - immutable way to update an array
+        //and push manipulates the original value
 
-                // concat returns a new array which is the older array plus the argument you add to concat - immutable way to update an array
-                // and push manipulates the original value
-            }
+        // return {
+        //     ...state,
+        //     results: state.results.concat({ id: new Date(), value: action.resultAct });
+        //     //results: state.results.concat({ id: new Date(), value: action.resultAct * 3 })
+        //     //have cases where you really want to change something before you store it in the state,
+        //     //Can change it in the Reducer(better way because is about state) ex multiple value result * 3 as u can see it above or in Actions has another way example
+        // }
         case actionTypes.DELETE_RESULT:
             const updateArray = state.results.filter(result => result.id !== action.resultElId);
-            return {
-                ...state,
-                results: updateArray
-            }
+            return updateObject(state, { results: updateArray })
+        // return {
+        //     ...state,
+        //     results: updateArray
+        // }
     }
     return state;
 }
