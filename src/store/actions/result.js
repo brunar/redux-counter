89feaf00259b50c0f0e97958f9cb7c71ca23b998
Bruna Radeke <brunaradeke@hotmail.com>
2020-05-13 19:26:57 +0100

    Redux Adv - Where to Put Data Transforming Logic? (16-291)
    to transforming the data better way keep more logic about state into the Reducer than into Actions Creators

--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -2,9 +2,11 @@ import * as actionTypes from './actionTypes';
 
 //Synchronous Functions action dispatch after 2s and may edit the store
 export const saveResult = (res) => {
+    //const updateResult = res * 2
     return {
         type: actionTypes.STORE_RESULT,
         resultAct: res
+        //resultAct: updateResult
     };
 };
 //Example SetTimeout 
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -7,9 +7,14 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
+            //or here can change data 
             return {
                 ...state,
                 results: state.results.concat({ id: new Date(), value: action.resultAct })
+                //results: state.results.concat({ id: new Date(), value: action.resultAct * 3 })
+                //have cases where you really want to change something before you store it in the state,
+                //Can change it in the Reducer(better way because is about state) ex multiple value result * 3 as u can see it above or in Actions has another way example
+
                 // concat returns a new array which is the older array plus the argument you add to concat - immutable way to update an array
                 // and push manipulates the original value
             }
