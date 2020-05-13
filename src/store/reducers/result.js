7c950edc29b8f4bd9e15a3dabac6011899b8170f
Bruna Radeke <brunaradeke@hotmail.com>
2020-05-13 20:05:51 +0100

    Redux Adv - A Leaner Switch Case Statement (16-294)
    shortcode for switch case

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,28 +5,17 @@ const initialState = {
     results: []
 }
 
+const deleteResult = (state, action) => {
+    const updateArray = state.results.filter(result => result.id !== action.resultElId);
+    return updateObject(state, { results: updateArray })
+}
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.STORE_RESULT:
-            //or here can change data 
-            return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.resultAct }) })
         // concat returns a new array which is the older array plus the argument you add to concat - immutable way to update an array
         //and push manipulates the original value
-
-        // return {
-        //     ...state,
-        //     results: state.results.concat({ id: new Date(), value: action.resultAct });
-        //     //results: state.results.concat({ id: new Date(), value: action.resultAct * 3 })
-        //     //have cases where you really want to change something before you store it in the state,
-        //     //Can change it in the Reducer(better way because is about state) ex multiple value result * 3 as u can see it above or in Actions has another way example
-        // }
-        case actionTypes.DELETE_RESULT:
-            const updateArray = state.results.filter(result => result.id !== action.resultElId);
-            return updateObject(state, { results: updateArray })
-        // return {
-        //     ...state,
-        //     results: updateArray
-        // }
+        case actionTypes.STORE_RESULT: return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.resultAct }) })
+        case actionTypes.DELETE_RESULT: return deleteResult(state, action);
     }
     return state;
 }
